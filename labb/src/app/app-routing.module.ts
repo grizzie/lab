6892b6187ecab2da8d5b3ac1c637a279eb152984
grizzie <capricorn.jyeyi@gmail.com>
2021-07-05 14:08:45 +0800

    responsive content

--- labb/src/app/app-routing.module.ts
+++ labb/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ProfessorComponent } from './professor/professor.component';
+import { MembersComponent } from './members/members.component';
+
 const routes: Routes = [
   { path: 'home',component: AboutusComponent},
   { path: 'services',component: AboutusComponent},
   { path: 'professor',component: ProfessorComponent},
   { path: 'aboutus',component: AboutusComponent},
-  { path: 'signup',component: AboutusComponent},
+  { path: 'members',component: MembersComponent},
 ];
 
 @NgModule({
--- labb/src/app/app.module.ts
+++ labb/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AboutusComponent } from './aboutus/aboutus.component';
 import { ProfessorComponent } from './professor/professor.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarouselPauseComponent } from './carousel-pause/carousel-pause.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { CarouselPauseComponent } from './carousel-pause/carousel-pause.componen
     NavbarComponent,
     AboutusComponent,
     ProfessorComponent,
-    CarouselPauseComponent
+    CarouselPauseComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
