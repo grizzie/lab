2c12b6bcdd1be14b7dc1cdf6c300a1de24a9ff02
grizzie <capricorn.jyeyi@gmail.com>
2021-07-07 12:07:51 +0800

    current-member-done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfessorComponent } from './professor/professor.component';
 import { MembersComponent } from './members/members.component';
 import { AboutComponent } from './about/about.component';
+import {CarouselPauseComponent } from './carousel-pause/carousel-pause.component';
+import { CurrentMembersComponent } from './members/current-members/current-members.component';
 const routes: Routes = [
+  { path: 'home',component: CarouselPauseComponent},
   { path: 'professor',component: ProfessorComponent},
   { path: 'about',component: AboutComponent},
   { path: 'members',component: MembersComponent},
+  { path: 'members/current-members',component: CurrentMembersComponent},
 ];
 
 @NgModule({
--- src/app/members/current-members/current-members.component.ts
+++ src/app/members/current-members/current-members.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import namelist from 'C:\\Users\\User.DESKTOP-NA2F2N8\\projects\\angular\\labb\\src\\app\\_files\\members-namelist.json';
 @Component({
   selector: 'app-current-members',
   templateUrl: './current-members.component.html',
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentMembersComponent implements OnInit {
 
-  constructor() { }
-
+  public students:{name:string, sex:string, field:string, study:string}[] = namelist;
+  constructor() {
+  }
   ngOnInit(): void {
   }
 
